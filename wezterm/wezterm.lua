bc4e64ffb4b0bde5dfbcc6eb09d2f99a7c9e6a34
Dinesh Kumar G <gdineshda@gmail.com>
2023-04-30 09:02:40 +0530

    Weekly Commit

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,7 +9,8 @@ end
 config.enable_scroll_bar = false
 config.enable_tab_bar = false
 config.color_scheme = 'Gruvbox dark, hard (base16)'
-config.font = wezterm.font 'Iosevka'
+config.font = wezterm.font 'Iosevka Term'
+config.font_size = 15.0
 config.cursor_blink_rate = 0
 config.window_padding = {
 	left	= 40,
