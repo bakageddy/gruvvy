67d2a74e66a5ff42eaa0f2ad4f2ef0fa7d1b74f3
Dinesh Kumar G <gdineshda@gmail.com>
2023-06-02 22:33:35 +0530

    Weekly Commit

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,14 +12,17 @@ config.enable_kitty_graphics = true
 config.enable_scroll_bar = false
 config.enable_tab_bar = false
 config.color_scheme = 'Gruvbox dark, hard (base16)'
-config.font = wezterm.font 'Iosevka Term'
+config.font = wezterm.font_with_fallback({
+	"Iosevka Term",
+	{ family = "Iosevka Nerd Font", scale = 0.75 }
+})
 config.font_size = 15.0
 config.cursor_blink_rate = 0
 config.window_padding = {
-	left	= 40,
-	right	= 40,
-	top		= 30,
-	bottom	= 30,
+	left   = 40,
+	right  = 40,
+	top    = 30,
+	bottom = 30,
 }
 
 return config
