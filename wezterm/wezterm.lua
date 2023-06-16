67d398f53e23f14c26060b00c707d3ec2e5773f8
Dinesh Kumar G <gdineshda@gmail.com>
2023-06-17 00:10:28 +0530

    Kanagawa

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,6 +6,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- config.window_background_opacity = 0.7
 config.adjust_window_size_when_changing_font_size = false
 config.animation_fps = 1
 config.enable_kitty_graphics = true
@@ -13,7 +14,7 @@ config.enable_scroll_bar = false
 config.enable_tab_bar = false
 config.color_scheme = 'kanagawabones'
 config.font = wezterm.font_with_fallback({
-	"Iosevka Term",
+	"Iosevka",
 	{ family = "Iosevka Nerd Font", scale = 0.75 }
 })
 config.font_size = 15.0
