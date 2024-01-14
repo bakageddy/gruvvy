edbf4f06312d8f2bbe7b60afdb6fe47f9eb46081
Dinesh Kumar G <gdineshda@gmail.com>
2024-01-14 21:17:59 +0530

    Monthly commit

--- doom/config.el
+++ doom/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Dinesh Kumar G"
+      user-mail-address "bakageddy@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+(setq doom-font (font-spec :family "Iosevka" :size 18 :weight 'medium)
+     doom-variable-pitch-font (font-spec :family "Iosevka" :size 18))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -32,15 +32,15 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-vibrant)
+(setq doom-theme 'doom-gruvbox)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type nil)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Documents/Org/")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,7 +12,7 @@ config.animation_fps = 1
 config.enable_kitty_graphics = true
 config.enable_scroll_bar = false
 config.enable_tab_bar = false
-config.color_scheme = 'kanagawabones'
+config.color_scheme = "Gruvbox dark, hard (base16)"
 config.font = wezterm.font_with_fallback({
 	"Iosevka",
 	{ family = "Iosevka Nerd Font", scale = 0.75 }
